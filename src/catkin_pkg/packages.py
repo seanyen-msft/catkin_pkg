857348c41fec1ad2f75e89f5b4b1753792dfc244
Sean Yen [MSFT] <seanyen@microsoft.com>
2019-02-11 12:58:43 -0800

    Disable for testing

--- src/catkin_pkg/packages.py
+++ src/catkin_pkg/packages.py
@@ -144,7 +144,7 @@ def find_packages_allowing_duplicates(basepath, exclude_paths=None, exclude_subs
     if not data:
         return {}
 
-    parallel = len(data) > 100
+    parallel = False
     if parallel:
         try:
             pool = multiprocessing.Pool()
