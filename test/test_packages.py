f56f001561c1d50cac91fd8989a97d2ab93e47a4
Sean Yen <seanyen@microsoft.com>
2019-02-11 14:11:35 -0800

    add a new test case.

--- test/test_packages.py
+++ test/test_packages.py
@@ -58,3 +58,10 @@ def test_find_packages_invalid_version():
         exception_message = str(e)
         assert version in exception_message
         assert path in exception_message
+
+@in_temporary_directory
+def test_find_packages_with_large_amount_packages():
+    for x in range(500):
+        _create_pkg_in_dir("test%s" % x)
+
+    find_package_paths('.')
